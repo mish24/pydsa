457224e2f770806dcab921cab0c9d686c981ba20
Poonam Mishra <poonammishra24398@gmail.com>
2016-12-19 01:39:32 +0530

    Add files via upload

---
+++ pydsa/cocktail_sort.py
@@ -0,0 +1,22 @@
+#cocktail sort
+
+def sort(s):
+    lower_bound = -1
+    upper_bound = len(s) - 1
+    swap = True
+    while swap:
+        swap = False
+        lower_bound += 1
+        for i in range(lower_bound, upper_bound):
+            if s[i] > s[i + 1]:
+                s[i], s[i + 1] = s[i + 1], s[i]
+                swap = True
+        if not swap:
+            break
+        swap= False
+        upper_bound -= 1
+        for i in range(upper_bound, lower_bound, -1):
+            if s[i] < s[i - 1]:
+                s[i], s[i - 1] = s[i - 1], s[i]
+                swap = True
+return s
---
+++ pydsa/test_cocktail_sort.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def sort(s):
+    lower_bound = -1
+    upper_bound = len(s) - 1
+    swap = True
+    while swap:
+        swap = False
+        lower_bound += 1
+        for i in range(lower_bound, upper_bound):
+            if s[i] > s[i + 1]:
+                s[i], s[i + 1] = s[i + 1], s[i]
+                swap = True
+        if not swap:
+            break
+        swap= False
+        upper_bound -= 1
+        for i in range(upper_bound, lower_bound, -1):
+            if s[i] < s[i - 1]:
+                s[i], s[i - 1] = s[i - 1], s[i]
+                swap = True
+        return s
+
+def test():
+    a = b = [randint(1, 100) for i in range(100)]
+    assert cocktail_sort(a) == sorted(b)
